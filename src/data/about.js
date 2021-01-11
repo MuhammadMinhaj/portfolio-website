4dc46ab15690c447e7aa376b86f8b5cb4c15fbf7
muhammad-minhaj <programmer.mdminhaj@gmail.com>
2021-01-11 19:16:45 +0600

    Added my photo in public/images

--- src/data/about.js
+++ src/data/about.js
@@ -6,7 +6,7 @@
  * **/
 
 const about = {
-  imgSource: "/static/images/5.jpg",
+  imgSource: "/static/images/mdminhaj.jpg",
   text:
     "I'm Muhammad Minhaj, a full-stack web and mobile application developer. I develop mobile and web based applications. I'm an expert in Javascript. I have the core skill in Javascript and I love to do most of the things developed using Javascript. All most, I am a Javascript lover. And I also an expert in Django framework of python for use in the back-end. Thank you so much for read about.",
 };
