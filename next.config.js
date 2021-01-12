5d48db1e717daafbd0be3cbd38e0e56b2d854bee
muhammad-minhaj <programmer.mdminhaj@gmail.com>
2021-01-12 23:59:14 +0600

    Added nextjs assetPrefix and basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const isDev = process.env.NODE_ENV === "development";
+
 module.exports = {
   env: {
     HEROKU_API_KEY: process.env.NEXT_PUBLIC_API_KEY || "",
     CONTACT_SEND_MAIL_URI_POST: process.env.NEXT_PUBLIC_CONTACT_API_URI || "",
   },
+
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isDev ? "" : "https://dev-mdminhaj.github.io",
+  basePath: isDev ? "" : "https://dev-mdminhaj.github.io",
 };
