c8ee21aa388dcc6537e68b7a2680e05eca995f5e
Muhammad Minhaj <programmer.mdminhaj@gmail.com>
2021-01-14 15:51:35 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    HEROKU_API_KEY: "developer@mdminhaj24ctg24&&mdminhaj-portfolio-web-api",
+    HEROKU_API_KEY: "",
     CONTACT_SEND_MAIL_URI_POST:
       "https://dev-mdminhaj.herokuapp.com/api/contact",
   },
