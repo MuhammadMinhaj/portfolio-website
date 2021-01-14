169557234a91f3b8ad636c73a82cac6143354a74
muhammad-minhaj <programmer.mdminhaj@gmail.com>
2021-01-14 12:41:24 +0600

    remove Next prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   env: {
-    HEROKU_API_KEY: process.env.NEXT_PUBLIC_API_KEY || "",
-    CONTACT_SEND_MAIL_URI_POST: process.env.NEXT_PUBLIC_CONTACT_API_URI || "",
+    HEROKU_API_KEY: process.env.PUBLIC_API_KEY || "",
+    CONTACT_SEND_MAIL_URI_POST: process.env.PUBLIC_CONTACT_API_URI || "",
   },
 };
