99620afbe8480501a504d1a6135c4545b50f6ff1
dev-mdminhaj <developer.mdminhaj@gmail.com>
2021-05-06 16:42:29 +0000

    Deploying to gh-pages from  @ c9c9af8622a90e96411c5e7f2adcb4399987e24a 🚀

---
+++ _next/static/tIOU7RUtJcELD226s2NHC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Faccb7e3e12eb5a4880ee.css","static\u002Fchunks\u002Fpages\u002Findex-3841ea07a6d098b2cd7f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5ad2de3ab6b8cfdbbde8.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
