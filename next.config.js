9924bad068b4690c7966842124db7e6f24d32c97
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-02-20 21:58:50 +0600

    fix: rmv env verify

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
   env: {
     HEROKU_API_KEY: "muhammadMinhajContactApiKey",
     CONTACT_SEND_MAIL_URI_POST:
       "https://dev-mdminhaj.herokuapp.com/api/contact",
   },
-  basePath: isProd ? "https://muhammadminhaj.github.io/portfolio" : "",
-  assetPrefix: isProd ? "https://muhammadminhaj.github.io/portfolio" : "",
+  basePath: "https://muhammadminhaj.github.io/portfolio",
+  assetPrefix: "https://muhammadminhaj.github.io/portfolio",
 };
