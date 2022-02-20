c11884dac292e6dbf16ba03bae197de828e6e450
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-02-20 21:35:29 +0600

    feat: Added asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   env: {
     HEROKU_API_KEY: "muhammadMinhajContactApiKey",
     CONTACT_SEND_MAIL_URI_POST:
       "https://dev-mdminhaj.herokuapp.com/api/contact",
   },
+  assetPrefix: isProd ? "https://cdn.mydomain.com" : "",
 };
